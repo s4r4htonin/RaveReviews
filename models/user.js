888369e23f38cb63f5e3949e7a8014e16e6e0e67
Sarah Thomas <s4r4hthomas@gmail.com>
2020-02-08 20:43:28 -0500

    Added .formHeader & .formStyling

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose"),
 //Schema set up
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    isAdmin: {type: Boolean, default: false} 
 });
 
 userSchema.plugin(passportLocalMongoose); //adds in methods to user for local authentication using mongoose
